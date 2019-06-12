401e673eb1475ec6b237727d6bbf8eec2ffb4c59
David Kanenwisher <d.kanen@gmail.com>
2019-06-11 21:46:42 -0500

    Add tests for IsItLoveYet.

---
+++ Is It Love?Tests/IsItLoveYetTests.swift
@@ -0,0 +1,63 @@
+//
+//  IsItLoveYetTests.swift
+//  Is It Love?Tests
+//
+//  Created by David Kanenwisher on 6/11/19.
+//  Copyright © 2019 David Kanenwisher. All rights reserved.
+//
+
+import XCTest
+@testable import Is_It_Love_
+
+class IsItLoveYetTests: XCTestCase {
+
+    var domain: IsItLoveYet!
+
+    override func setUp() {
+       domain = IsItLoveYet()
+    }
+
+    func test_it_starts_at_0() {
+
+        XCTAssertEqual(0.0, domain.currentProgress())
+        XCTAssertEqual(false, domain.hasReachedLoveLimit())
+    }
+
+    func test_it_increments_by_10() {
+
+        domain.incrementProgress()
+
+        XCTAssertEqual(10.0, domain.currentProgress())
+    }
+
+    func test_it_decrements_by_10() {
+
+        for _ in 1...2 {
+            domain.incrementProgress()
+        }
+        XCTAssertEqual(20.0, domain.currentProgress())
+
+        domain.decrementProgress()
+
+        XCTAssertEqual(10.0, domain.currentProgress())
+    }
+
+    func test_it_resets_to_0() {
+
+        domain.incrementProgress()
+
+        domain.resetProgress()
+
+        XCTAssertEqual(0.0, domain.currentProgress())
+    }
+
+    func test_it_can_reach_the_love_limit() {
+
+        for _ in 1...10 {
+            domain.incrementProgress()
+        }
+        XCTAssertEqual(100.0, domain.currentProgress())
+
+        XCTAssertEqual(true, domain.hasReachedLoveLimit())
+    }
+}
